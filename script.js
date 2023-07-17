deb4255c62052d0489c94ab5dc9096c38e5fc4b1
unknown <shanelwilliams2018@gmail.com>
2023-07-17 15:08:13 -0600

    added function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /* Add an easy fun fun function here if you like, but you don't have to */
+
+function justCuz() {
+    let body = document.querySelector('body')
+    let newDiv = document.createElement('div')
+    let text = document.createTextNode('Hi Dina! This type of random project is something I have wanted to do for a while.')
+    newDiv.classList.add('text')
+    body.appendChild(newDiv)
+    newDiv.appendChild(text)
+}
+
+justCuz()
